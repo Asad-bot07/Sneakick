652504dc5fa5406742b5f9fd9e7a0549408700da
Asad-bot07 <asadhussain2408@gmail.com>
2025-07-08 21:27:39 +0530

    coded

---
+++ script.js
@@ -0,0 +1,14 @@
+const carousel = document.getElementById("carousel");
+    const slides = carousel.querySelectorAll("img");
+    const totalSlides = slides.length;
+    let index = 0;
+
+    document.getElementById("next").addEventListener("click", () => {
+      index = (index + 1) % totalSlides;
+      carousel.style.transform = `translateX(-${index * 100}%)`;
+    });
+
+    document.getElementById("prev").addEventListener("click", () => {
+      index = (index - 1 + totalSlides) % totalSlides;
+      carousel.style.transform = `translateX(-${index * 100}%)`;
+    });
