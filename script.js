c05e1ab08a138283269c362de44a384443d43f96
Asad-bot07 <asadhussain2408@gmail.com>
2025-07-09 01:55:53 +0530

    Updated javascript for popup

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const carousel = document.getElementById("carousel");
+document.addEventListener('DOMContentLoaded',()=>{
+    const carousel = document.getElementById("carousel");
     const slides = carousel.querySelectorAll("img");
+    const menu= document.getElementById('menu');
+    menu.addEventListener('click',()=>{
+      document.getElementById('PopUp').classList.add('opacity-100','scale-95')
+      document.getElementById('PopUp').classList.remove('opacity-0','scale-0')
+    })
     const totalSlides = slides.length;
     let index = 0;
 
@@ -12,3 +18,4 @@ const carousel = document.getElementById("carousel");
       index = (index - 1 + totalSlides) % totalSlides;
       carousel.style.transform = `translateX(-${index * 100}%)`;
     });
+  });
